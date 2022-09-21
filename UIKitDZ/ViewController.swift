c90892adde453b882e529a61b1678192d2695d1d
muxammed <muxammedsaparow@gmail.com>
2022-09-21 17:17:25 +0300

    test

--- UIKitDZ/ViewController.swift
+++ UIKitDZ/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .yellow
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
