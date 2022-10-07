923a7842eeb7aa320e9126b9c491a4d880911f3b
muxammed <muxammedsaparow@gmail.com>
2022-10-07 09:13:17 +0300

    Урок 14 сделанный

--- UIKitDZ/ViewController.swift
+++ UIKitDZ/ViewController.swift
@@ -6,36 +6,256 @@
 //
 
 import UIKit
-/// это тестовый ViewController
+
+/// Константы для проекта
+struct Constants {
+    static let dummyText = "Lorem ipsum dolor sit amet,"
+    + " consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. "
+    + "Cum sociis natoque penatibus et  magnis dis parturient montes, nascetur ridiculus mus. "
+    + "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. "
+    + "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, "
+    + "aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, "
+    + "venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. "
+    + "Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. "
+    + "Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, "
+    + "consequat vitae, eleifend ac, enim. Aliquam lorem ante, "
+    + "dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus "
+    + " varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. "
+    + "Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. "
+    + "Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, "
+    + "sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, "
+    + "luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt"
+    + " tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. "
+    + "Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. "
+    + "Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. "
+    + "Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,"
+}
+
+/// ViewController с TextView и прочими элементами настройки
 class ViewController: UIViewController {
 
+    // MARK: - Visual Components
+    var myTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.systemFont(ofSize: 17, weight: .thin)
+        textView.text = Constants.dummyText
+        textView.textColor = .black
+        textView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        return textView
+    }()
+    
+    var textSizeSlider: UISlider = {
+        let slider = UISlider()
+        slider.maximumValue = 100
+        slider.minimumValue = 10
+        slider.value = 17
+        slider.tintColor = .orange
+        return slider
+    }()
+    
+    var smallSizeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "A"
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textColor = .orange
+        return label
+    }()
+    
+    var bigSizeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "A"
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.textColor = .orange
+        return label
+    }()
+    
+    var blackColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    var greenColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGreen
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    var blueColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    var grayColorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    var thinnerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Thinner", for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    var bolderButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Bolder", for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    var shareTextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("", for: .normal)
+        button.setBackgroundImage(UIImage(systemName: "square.and.arrow.up.fill"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.backgroundColor = .white
+        button.tintColor = .systemIndigo
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    var lightDarkSwitcher: UISwitch = {
+        let switcher = UISwitch()
+        switcher.tintColor = .darkGray
+        return switcher
+    }()
+    
+    var fontPickerView: UIPickerView = {
+        let picker = UIPickerView()
+        return picker
+    }()
+    
+    // MARK: - Public Properties
+    let weights = [UIFont.Weight.ultraLight, UIFont.Weight.light,
+                   UIFont.Weight.thin, UIFont.Weight.regular, UIFont.Weight.medium,
+                   UIFont.Weight.bold, UIFont.Weight.black, UIFont.Weight.heavy]
+    var curWeight = 2
+    
+    // MARK: - UIViewController(*)
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        configure()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        myTextView.frame = CGRect(x: 0,
+                                  y: self.view.safeAreaInsets.top,
+                                  width: self.view.bounds.width, height: self.view.bounds.height / 2)
+        textSizeSlider.frame = CGRect(x: 0, y: myTextView.frame.maxY + 20,
+                                      width: view.frame.width / 2, height: textSizeSlider.frame.height)
+        textSizeSlider.center.x = view.center.x
+        smallSizeLabel.frame = CGRect(x: textSizeSlider.frame.minX - 20, y: 0, width: 20, height: 20)
+        bigSizeLabel.frame = CGRect(x: textSizeSlider.frame.maxX + 20, y: 0, width: 20, height: 20)
+        smallSizeLabel.center.y = textSizeSlider.center.y
+        bigSizeLabel.center.y = textSizeSlider.center.y
+        
+        blackColorView.frame = CGRect(x: 0, y: textSizeSlider.frame.maxY + 50,
+                                      width: (view.frame.width / 4) - 30, height: (view.frame.width / 4) - 30)
+        blackColorView.layer.cornerRadius = blackColorView.frame.width / 2
+        blackColorView.center.x = (view.frame.width / 4) / 2
+        
+        greenColorView.frame = CGRect(x: 0, y: textSizeSlider.frame.maxY + 50,
+                                      width: (view.frame.width / 4) - 30, height: (view.frame.width / 4) - 30)
+        greenColorView.layer.cornerRadius = blackColorView.frame.width / 2
+        greenColorView.center.x = ((view.frame.width / 4) / 2) * 3
+        
+        blueColorView.frame = CGRect(x: 0, y: textSizeSlider.frame.maxY + 50,
+                                      width: (view.frame.width / 4) - 30, height: (view.frame.width / 4) - 30)
+        blueColorView.layer.cornerRadius = blackColorView.frame.width / 2
+        blueColorView.center.x = ((view.frame.width / 4) / 2) * 5
+        
+        grayColorView.frame = CGRect(x: 0, y: textSizeSlider.frame.maxY + 50,
+                                      width: (view.frame.width / 4) - 30, height: (view.frame.width / 4) - 30)
+        grayColorView.layer.cornerRadius = blackColorView.frame.width / 2
+        grayColorView.center.x = ((view.frame.width / 4) / 2) * 7
+        
+        thinnerButton.frame = CGRect(x: 10, y: blackColorView.frame.maxY + 50,
+                                     width: (view.frame.width / 2) - 20, height: 44)
+        bolderButton.frame = CGRect(x: view.frame.width / 2, y: blackColorView.frame.maxY + 50,
+                                     width: (view.frame.width / 2) - 20, height: 44)
+        shareTextButton.frame = CGRect(x: 10, y: bolderButton.frame.maxY + 10, width: 40, height: 40)
     }
 
-    // MARK: - IBOutlet
-
-    // MARK: - Visual Components
-
-    // MARK: - Public Properties
-
-    // MARK: - Private Properties
-
-    // MARK: - Initializers
-
-    // MARK: - UIViewController(*)
-
     // MARK: - Public methods
-
-    // MARK: - IBAction
+    @objc func makeThinner() {
+        if curWeight > 1 {
+            curWeight -= 1
+            myTextView.font = UIFont.systemFont(ofSize: myTextView.font?.pointSize ?? 17, weight: weights[curWeight])
+        }
+    }
+    
+    @objc func shareButtonAction() {
+        let activityViewController = UIActivityViewController(activityItems: [Constants.dummyText],
+                                                              applicationActivities: nil)
+            present(activityViewController, animated: true, completion: nil)
+                
+    }
+    
+    @objc func makeBolder() {
+        print("\(curWeight)")
+        if curWeight < (weights.count - 1) {
+            curWeight += 1
+            myTextView.font = UIFont.systemFont(ofSize: myTextView.font?.pointSize ?? 17, weight: weights[curWeight])
+        }
+    }
+    
+    @objc func setBlackColor(_ sender: UIView) {
+        myTextView.textColor = .black
+    }
+    
+    @objc func setGreenColor(_ sender: UIView) {
+        myTextView.textColor = .green
+    }
+    
+    @objc func setBlueColor(_ sender: UIView) {
+        myTextView.textColor = .blue
+    }
+    
+    @objc func setGrayColor(_ sender: UIView) {
+        myTextView.textColor = .gray
+    }
+    
+    @objc func sliderSlidedAction(_ sender: UISlider) {
+        myTextView.font = UIFont(name: myTextView.font?.fontName ?? "Helvetica", size: CGFloat(sender.value))
+    }
 
     // MARK: - Private Methods
-
-    // MARK: - Types
-
-    // MARK: - Constants
-
+    fileprivate func configure() {
+        view.addSubview(myTextView)
+        view.addSubview(smallSizeLabel)
+        view.addSubview(textSizeSlider)
+        view.addSubview(bigSizeLabel)
+        view.addSubview(blackColorView)
+        view.addSubview(greenColorView)
+        view.addSubview(blueColorView)
+        view.addSubview(grayColorView)
+        view.addSubview(thinnerButton)
+        view.addSubview(bolderButton)
+        view.addSubview(shareTextButton)
+        
+        textSizeSlider.addTarget(self, action: #selector(sliderSlidedAction(_:)), for: .valueChanged)
+        blackColorView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(setBlackColor(_:))))
+        blueColorView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(setBlueColor(_:))))
+        greenColorView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(setGreenColor(_:))))
+        grayColorView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(setGrayColor(_:))))
+        
+        thinnerButton.addTarget(self, action: #selector(makeThinner), for: .touchUpInside)
+        bolderButton.addTarget(self, action: #selector(makeBolder), for: .touchUpInside)
+        shareTextButton.addTarget(self, action: #selector(shareButtonAction), for: .touchUpInside)
+    }
 }
