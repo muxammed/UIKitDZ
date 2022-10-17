0602dca5e12050715a6680699579a83ca8634bc1
muxammed <muxammedsaparow@gmail.com>
2022-10-17 05:48:38 +0300

    Урок 12 сделан

--- UIKitDZ/ViewController.swift
+++ UIKitDZ/ViewController.swift
@@ -6,36 +6,42 @@
 //
 
 import UIKit
-/// это тестовый ViewController
-class ViewController: UIViewController {
 
+/// Константы 
+struct Constants {
+    static let tabOnaName = "First Tab"
+    static let tabTwoName = "Second Tab"
+    static let tabOneIcon = "tray.fill"
+    static let tabTwoIcon = "archivebox.fill"
+    static let plusIcon = "plus"
+    static let okeyText = "Готово"
+    static let placeholderText = "Введите текст"
+    static let alertText = "Введите текст для лейбла"
+}
+
+/// это тестовый ViewController
+class ViewController: UITabBarController {
+
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        configure()
     }
-
-    // MARK: - IBOutlet
-
-    // MARK: - Visual Components
-
-    // MARK: - Public Properties
-
-    // MARK: - Private Properties
-
-    // MARK: - Initializers
-
-    // MARK: - UIViewController(*)
-
-    // MARK: - Public methods
-
-    // MARK: - IBAction
-
-    // MARK: - Private Methods
-
-    // MARK: - Types
-
-    // MARK: - Constants
-
+    
+    // MARK: - Private method
+    private func configure() {
+        view.backgroundColor = .white
+        let firstTab = UINavigationController(rootViewController: FirstTabController())
+        let secondTab = UINavigationController(rootViewController: SecondTabController())
+        let icon1 = UITabBarItem(title: Constants.tabOnaName, image: UIImage(systemName: Constants.tabOneIcon),
+                                 selectedImage: UIImage(systemName: Constants.tabOneIcon))
+        firstTab.tabBarItem = icon1
+        let icon2 = UITabBarItem(title: Constants.tabTwoName, image: UIImage(systemName: Constants.tabTwoIcon),
+                                 selectedImage: UIImage(systemName: Constants.tabTwoIcon))
+        secondTab.tabBarItem = icon2
+        tabBar.tintColor = .white
+        tabBar.backgroundColor = .black
+        tabBar.barTintColor = .darkGray
+        setViewControllers([firstTab, secondTab], animated: true)
+    }
 }
